5f4284a0d33d508b1fdfa2f3ba0ff74b3bf32aa6
Işıl Yapıcı <72387793+isilypici@users.noreply.github.com>
2021-01-10 21:03:03 +0300

    PieChart Test

--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,45 @@
 module Main exposing (..)
 
+--import BarChart exposing (..)
+import PieChart exposing (..)
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (style)
+import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-import BarChart exposing (..)
-
-
 -- MAIN
-
-
 main =
-  Browser.sandbox { init = init, update = update, view = view }
-
+  Browser.sandbox
+  { init = init
+  , update = update
+  , view = view
+  }
 
 
 -- MODEL
+type alias Model =
+  { content : String
+  }
 
-type alias Model = Int
-
-type User =
-  Regular {name : String, age: Int }
-  | Anonymous
 
 init : Model
 init =
-  0
-
+  { content = "" }
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg
+  = Change String
+  | Show
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
+    Change newContent ->
+      { model | content = newContent }
+    Show ->
+        { model | content = "welcome"}
 
 -- VIEW
 
@@ -51,27 +49,27 @@ view model =
     div [style "text-align" "center"
                , style "background-color" "rgb(44 53 49)"
                , style "color" "rgb(255 255 255)"
-               , style "font-size" "4em" ][ text "Industrial Facility Tracing System" ],
-    div [style "background-color" "rgb(216 176 140)"]
-        [ b [ style "font-size" "2em"
-            , style "padding-left" "10cm"][ text "Menu A"],
-          b [ style "font-size" "2em"
-            , style "padding-left" "10cm"][ text "Menu B"],
-          b [ style "font-size" "2em"
-            , style "padding-left" "10cm"][ text "Menu C"]],
-    br[][],
-    span[style "background-color" "rgb(210 233 226)"][span [ style "font-size" "2em", style "padding-left" "5cm"
+               , style "font-size" "4em" ][ text "Industrial Facility Tracing System" ]
+    , div [style "background-color" "rgb(216 176 140)"]
+        [ span[style "padding-left" "8cm"][],
+          button [ style "font-size" "2em", style "padding" "2px 30px"
+            , onClick Show ][text "Menu A"] , span[style "padding-left" "8cm"][],
+          button [ style "font-size" "2em", style "padding" "2px 30px"
+            , onClick Show][ text "Menu B"], span[style "padding-left" "8cm"][],
+          button [ style "font-size" "2em", style "padding" "2px 30px"
+            , onClick Show][ text "Menu C"]]
+    , br[][]
+    , span[style "background-color" "rgb(210 233 226)"][button [ style "font-size" "2em", style "padding-left" "4cm"
                                                            , style "text-align" "top"][text "Menu 1"],
                                                       br[][],
-                                                          span[style "background-color" "rgb(210 233 226)"][span [ style "font-size" "2em", style "padding-left" "5cm"
-                                                           , style "text-align" "top"][text "Menu 1"],
-                                                      br[][],
-                                                      span [ style "font-size" "2em", style "padding-left" "5cm"
-                                                           , style "text-align" "top"][text "Menu 2"] ],
-    --graph part
-    span[][BarChart.main, p[style "padding-left" "5cm"][text "data 1 >> x"]]
+                                                      button [ style "font-size" "2em", style "padding-left" "4cm"
+                                                           , style "text-align" "top"][text "Menu 2"] ]
+    {-graph part
+    , span[][BarChart.main,
+           div[style "padding-left" "5cm"][text "data 1 >> x"]]
+-}
+    --, span[][PieChart.main]
 
     ]
 
 
-    
---
+++ src/PieChart.elm
@@ -0,0 +1,223 @@
+module PieChart exposing (main)
+
+import Array exposing (Array)
+import Browser
+import Color exposing (Color)
+import Html exposing (Html, br, div, h2, input, label)
+import Html.Attributes as Attr exposing (step, style, type_, value)
+import Html.Events exposing (onInput)
+import Path
+import Shape exposing (defaultPieConfig)
+import TypedSvg exposing (g, svg, text_)
+import TypedSvg.Attributes exposing (dy, fill, stroke, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (height, width)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), Paint(..), Transform(..), em)
+
+w : Float
+w =
+    400
+
+
+h : Float
+h =
+    100
+
+
+colors : Array Color
+colors =
+    Array.fromList
+        [ Color.rgb255 152 171 198
+        , Color.rgb255 138 137 166
+        , Color.rgb255 123 104 136
+        , Color.rgb255 107 72 107
+        , Color.rgb255 159 92 85
+        , Color.rgb255 208 116 60
+        , Color.rgb255 255 96 0
+        ]
+
+
+radius : Float
+radius =
+    min w h / 2
+
+
+type alias ChartConfig =
+    { outerRadius : Float
+    , innerRadius : Float
+    , padAngle : Float
+    , cornerRadius : Float
+    , labelPosition : Float
+    }
+
+
+type Msg
+    = UpdateOuterRadius String
+    | UpdateInnerRadius String
+    | UpdatePadAngle String
+    | UpdateCornerRadius String
+    | UpdateLabelPosition String
+
+
+type alias Model =
+    { config : ChartConfig
+    , data : List ( String, Float )
+    }
+
+
+drawChart : ChartConfig -> List ( String, Float ) -> Svg msg
+drawChart config model =
+    let
+        pieData =
+            model
+                |> List.map Tuple.second
+                |> Shape.pie
+                    { defaultPieConfig
+                        | innerRadius = config.innerRadius
+                        , outerRadius = config.outerRadius
+                        , padAngle = config.padAngle
+                        , cornerRadius = config.cornerRadius
+                        , sortingFn = \_ _ -> EQ
+                    }
+
+        makeSlice index datum =
+            Path.element (Shape.arc datum) [ fill <| Paint <| Maybe.withDefault Color.black <| Array.get index colors, stroke <| Paint Color.white ]
+
+        makeLabel slice ( label, value ) =
+            let
+                ( x, y ) =
+                    Shape.centroid { slice | innerRadius = config.labelPosition, outerRadius = config.labelPosition }
+            in
+            text_
+                [ transform [ Translate x y ]
+                , dy (em 0.35)
+                , textAnchor AnchorMiddle
+                ]
+                [ text label ]
+    in
+    svg [ width (radius * 2), height (radius * 2) ]
+        [ g [ transform [ Translate radius radius ] ]
+            [ g [] <| List.indexedMap makeSlice pieData
+            , g [] <| List.map2 makeLabel pieData model
+            ]
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    let
+        config =
+            model.config
+    in
+    case msg of
+        UpdateOuterRadius amount ->
+            { model | config = { config | outerRadius = Maybe.withDefault 0 <| String.toFloat amount } }
+
+        UpdateInnerRadius amount ->
+            { model | config = { config | innerRadius = Maybe.withDefault 0 <| String.toFloat amount } }
+
+        UpdatePadAngle amount ->
+            { model | config = { config | padAngle = Maybe.withDefault 0 <| String.toFloat amount } }
+
+        UpdateCornerRadius amount ->
+            { model | config = { config | cornerRadius = Maybe.withDefault 0 <| String.toFloat amount } }
+
+        UpdateLabelPosition amount ->
+            { model | config = { config | labelPosition = Maybe.withDefault 0 <| String.toFloat amount } }
+
+
+view : Model -> Html Msg
+view model =
+    div [ style "display" "flex", style "justify-content" "space-evenly" ]
+        [ drawChart model.config model.data
+        , div []
+            [ h2 [] [ text "Pie Configuration" ]
+            , label [] [ text "Outer Radius" ]
+            , input
+                [ type_ "range"
+                , onInput UpdateOuterRadius
+                , value (String.fromFloat model.config.outerRadius)
+                , Attr.min "0"
+                , Attr.max (String.fromFloat radius)
+                ]
+                []
+            , text (String.fromFloat model.config.outerRadius)
+            , br [] []
+            , label [] [ text "Inner Radius" ]
+            , input
+                [ type_ "range"
+                , onInput UpdateInnerRadius
+                , value (String.fromFloat model.config.innerRadius)
+                , Attr.min "0"
+                , Attr.max (String.fromFloat radius)
+                ]
+                []
+            , text (String.fromFloat model.config.innerRadius)
+            , br [] []
+            , label [] [ text "Pad Angle" ]
+            , input
+                [ type_ "range"
+                , onInput UpdatePadAngle
+                , value (String.fromFloat model.config.padAngle)
+                , Attr.min "0"
+                , Attr.max "0.8"
+                , step "0.01"
+                ]
+                []
+            , text (String.fromFloat model.config.padAngle)
+            , br [] []
+            , label [] [ text "Corner Radius" ]
+            , input
+                [ type_ "range"
+                , onInput UpdateCornerRadius
+                , value (String.fromFloat model.config.cornerRadius)
+                , Attr.min "0"
+                , Attr.max "20"
+                , step "0.25"
+                ]
+                []
+            , text (String.fromFloat model.config.cornerRadius)
+            , br [] []
+            , label [] [ text "Label Position" ]
+            , input
+                [ type_ "range"
+                , onInput UpdateLabelPosition
+                , value (String.fromFloat model.config.labelPosition)
+                , Attr.min "0"
+                , Attr.max (String.fromFloat radius)
+                ]
+                []
+            , text (String.fromFloat model.config.labelPosition)
+            , br [] []
+            ]
+        ]
+
+
+data : List ( String, Float )
+data =
+    [ ( "<5", 2704659 )
+    , ( "5-13", 4499890 )
+    , ( "14-17", 2159981 )
+    , ( "18-24", 3853788 )
+    , ( "25-44", 14106543 )
+    , ( "45-64", 8819342 )
+    , ( "≥65", 612463 )
+    ]
+
+
+init : { config : ChartConfig, data : List ( String, Float ) }
+init =
+    { config =
+        { outerRadius = 50
+        , innerRadius = 0
+        , padAngle = 0
+        , cornerRadius = 0
+        , labelPosition = 50
+        }
+    , data = data
+    }
+
+
+main : Program () { config : ChartConfig, data : List ( String, Float ) } Msg
+main =
+    Browser.sandbox{ init = init, update = update, view = view }
